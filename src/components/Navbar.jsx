fed57000d6a6eb3cfe983f983f738c2349fe02c2
Akash Kumar <akash19g245@gmail.com>
2025-11-29 18:15:47 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import dayjs from 'dayjs';
+
+import { navLinks, navIcons } from '#constants';
+import { useTheme } from '#context/ThemeContext.jsx';
+
+
+const Navbar = () => {
+    const { toggleTheme } = useTheme();
+
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className='font-bold'>Akash's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img
+                                src={img}
+                                alt={`icon-${id}`}
+                                className='icon-hover'
+                                onClick={id === 4 ? toggleTheme : undefined}
+                                style={id === 4 ? { cursor: 'pointer' } : {}}
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
