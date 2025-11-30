be9943190930f221c43979dab49102b52a4c6925
Akash Kumar <akash19g245@gmail.com>
2025-11-30 09:11:07 +0530

    implemented a hoc for managing window states and add the first terminal window

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
+import WindowControlls from '#components/WindowControlls'
 
 
 
-export {Navbar, Welcome, Dock};
+
+export {Navbar, Welcome, Dock, WindowControlls};
