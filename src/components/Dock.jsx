60399bfc5f33d9c3fb7aed09f0260dc59edc1f56
Akash Kumar <akash19g245@gmail.com>
2025-11-30 07:27:20 +0530

    implemented store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,8 +4,12 @@ import gsap from "gsap";
 
 import { dockApps } from "#constants";
 import { useGSAP } from "@gsap/react";
+import { useWindowStore } from "#store/window.js";
 
 const Dock = () => {
+
+    const {openWindow, closeWindow, windows } = useWindowStore();
+
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -59,7 +63,19 @@ const Dock = () => {
 
 
 
-  const toggleApp = (app) => {};
+  const toggleApp = (app) => {
+    if(!app.canOpen) return;
+
+    const window = windows[app.id]
+
+    if(window.isOpen) {
+        closeWindow(app.id);
+    } else {
+        openWindow(app.id);
+    }
+
+    console.log(windows);
+  };
 
   return (
     <section id="dock">
